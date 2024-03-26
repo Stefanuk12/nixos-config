b1dde22fba7a2138576a094291058c0e0639171d
Stefan <stefanukpadd@gmail.com>
2024-03-26 19:00:59 +0000

    setup vps

--- hosts/vps/configuration.nix
+++ hosts/vps/configuration.nix
@@ -13,13 +13,46 @@ in {
       ../../system/${hostName}
     ];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # Use the grub EFI boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";  
 
   # Setup networking
   networking.hostName = hostName; # Define your hostname.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.useDHCP = false;
+  systemd.network = {
+    enable = true;
+    networks."10-internet" = {
+      matchConfig.MACAddress = "00:50:56:45:1a:e6"; # Interface MAC address here
+      linkConfig.RequiredForOnline = "routable";
+
+      address = [
+        "38.242.201.72/24" # Your server IPv4 address here
+        # "2a02:c206:2078:9036:0000:0000:0000:0001/64" # Your server IPv6 address here
+      ];
+      routes = [
+          {
+            routeConfig = {
+                Gateway = "38.242.192.1"; # Your gateway address here
+                GatewayOnLink = true;
+            };
+          }
+          # {
+          #   routeConfig = {
+          #       Gateway = "fe80::1"; # This should be correct as is
+          #       GatewayOnLink = true;
+          #   };
+          # }
+      ];
+      dns = [
+        "161.97.189.52"
+        "161.97.189.51"
+        # "2a02:c206:5028::2:53"
+        # "2a02:c206:5028::1:53"
+      ];
+    };
+  };
 
   # Set your time zone.
   time.timeZone = timeZone;
@@ -53,4 +86,3 @@ in {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   system.stateVersion = "23.05";
 }
-
