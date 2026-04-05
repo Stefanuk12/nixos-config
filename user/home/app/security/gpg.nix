47325e1fdec5004bcc40c55aaa402b92e4044a4e
Stefan Petrovic <stefan@petrovic.foo>
2026-04-05 20:40:01 +0100

    cache gpg

--- user/home/app/security/gpg.nix
+++ user/home/app/security/gpg.nix
@@ -6,6 +6,10 @@
     enable = true;
     enableSshSupport = true;
     pinentry.package = pkgs.pinentry-qt;
+    defaultCacheTtl = 3600; # seconds (1 hour)
+    defaultCacheTtlSsh = 3600;
+    maxCacheTtl = 7200; # max regardless of activity (2 hours)
+    maxCacheTtlSsh = 7200;
   };
   home.sessionVariables = {
     SSH_AUTH_SOCK = "$(gpgconf --list-dirs agent-ssh-socket)";
