65f9be3f4af1a17a165e1491a018256556ade721
Stefan Petrovic <stefan@petrovic.foo>
2026-04-05 20:37:52 +0100

    ssh auth sock

--- user/home/app/security/gpg.nix
+++ user/home/app/security/gpg.nix
@@ -4,6 +4,10 @@
   programs.gpg.enable = true;
   services.gpg-agent = {
     enable = true;
+    enableSshSupport = true;
     pinentry.package = pkgs.pinentry-qt;
+  };
+  home.sessionVariables = {
+    SSH_AUTH_SOCK = "$(gpgconf --list-dirs agent-ssh-socket)";
   };
 }
